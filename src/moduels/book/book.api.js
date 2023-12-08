35c1d2c05499a16c58b52fcaa01cf4fcc907019f
Ahmed Nabil <132344473+ahmednabil4445@users.noreply.github.com>
2023-12-08 20:41:36 +0200

    Update book.api.js

--- src/moduels/book/book.api.js
+++ src/moduels/book/book.api.js
@@ -1,8 +1,8 @@
 const app = require('express').Router()
 const { uploadSingleImage } = require('../../middleware/fileUpload')
-const { protectedRoutes } = require('../auth/auth.service')
+const { protectedRoutes, allowedTo } = require('../auth/auth.service')
 const { createBook, getAllBooks, updateBook, deleteBook, getSpecificBook, getDetailsOfBook } = require('./book.service')
-app.route('/').post(protectedRoutes, uploadSingleImage('image', 'book'), createBook).get(getAllBooks)
+app.route('/').post(protectedRoutes,allowedTo('admin'), uploadSingleImage('image', 'book'), createBook).get(getAllBooks)
 app.route('/:id').get(getSpecificBook).delete(deleteBook).put(updateBook)
 app.route("/getDetailsOfbook/:id").get(getDetailsOfBook);
 
